2f99e37ef3dab0b519aa1cf4bce422dd77c65bd4
manzitresor <manzitre@gmail.com>
2025-06-18 10:17:50 +0200

    chore: add about section syles

--- src/page/Aboutsection.tsx
+++ src/page/Aboutsection.tsx
@@ -6,19 +6,19 @@ export default function Aboutsection() {
         <h4 className="text-3xl">About us</h4>
         <hr className="w-31 h-px my-3 bg-main-orange border-0"></hr>
       </div>
-      <div>
-        <div>
-            <p>
-              We are a specialized team committed to providing reliable car rental services. One of the advantages of renting a car from us is offering competitive and transparent prices.
-              By providing services such as comprehensive insurance ......
-            </p>
-            <span className="text-main-orange">
-              Read more 
-            </span>
-        </div>
-        <div>
-          <img src={car} alt="car" className="w-96 h-96 object-cover mx-auto mt-8" />
-        </div>
+      <div className='flex gap-5 mx-48 mt-5'>
+          <div className='w-1/2 h-56'>
+            <img src={car} alt="car" className='w-full h-full object-cover'/>
+          </div>
+          <div className='w-2/3 h-96'>
+              <p>
+                We are a specialized team committed to providing reliable car rental services. One of the advantages of renting a car from us is offering competitive and transparent prices.
+                By providing services such as comprehensive insurance ......
+              </p>
+              <span className="text-main-orange">
+                Read more 
+              </span>
+          </div>
       </div>
     </div>
   )
