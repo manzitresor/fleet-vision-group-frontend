49580bd2559151b54705b84d7cc58f0f0aa3ec35
manzitresor <manzitre@gmail.com>
2025-06-25 19:54:43 +0200

    chore: add icons in the card component

--- src/page/CarListSection.tsx
+++ src/page/CarListSection.tsx
@@ -1,3 +1,4 @@
+import FeaturedCard from "@/components/shared/featuredCard"
 import { Card, CardContent } from "@/components/ui/card"
 import {
     Carousel,
@@ -6,8 +7,7 @@ import {
     CarouselNext,
     CarouselPrevious,
   } from "@/components/ui/carousel"
-import imageOne from "../assets/2 1.svg"
-import  { Button } from "@/components/ui/button"
+
 
 export default function CarListSection() {
     return (
@@ -19,23 +19,7 @@ export default function CarListSection() {
                 <div className="p-1">
                   <Card>
                     <CardContent className="aspect-square p-3">
-                      <img src={imageOne} alt="carOne"/>
-                      <div className="my-4">
-                        <h6 className="font-medium">Jeep XD</h6>
-                        <ul className="flex gap-5">
-                          <li> 5</li>
-                          <li> Gasoline</li>
-                          <li> SUV</li>
-                        </ul>
-                      </div>
-                      <div className="flex justify-between">
-                        <p className="font-semibold text-base">Daily rate</p>
-                        <p className="font-bold text-main-orange">$25 - $35</p>
-                      </div>
-                      <div className="pt-9 flex justify-center gap-4">
-                        <Button className="bg-main-orange text-white px-8 cursor-pointer">Book now</Button>
-                        <Button className="px-8 cursor-pointer hover:bg-main-orange hover:text-white">Details</Button>
-                      </div>
+                      <FeaturedCard/>
                     </CardContent>
                   </Card>
                 </div>
