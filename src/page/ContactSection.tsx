7f898f5eabc5eee8acefa31e9cc9af0dc6bb7ed2
manzitresor <manzitre@gmail.com>
2025-07-02 23:55:51 +0200

    chore: add form and validation

--- src/page/ContactSection.tsx
+++ src/page/ContactSection.tsx
@@ -1,7 +1,31 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { useForm } from "react-hook-form"
+import type { SubmitHandler } from "react-hook-form"
+
+type Inputs = {
+  example: string
+  exampleRequired: string
+}
+
 export default function ContactSection() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>()
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
   return (
     <div>
-      <h2>Contact Us</h2>
+        <div className=" flex flex-col items-center justify-center">
+            <h4 className="text-2xl">Contact us</h4>
+            <hr className="w-31 h-px my-3 bg-main-orange border-0"></hr>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input defaultValue="test" {...register("example")} />
+          <Input {...register("exampleRequired", { required: true })} />
+          <Button></Button>
+        </form>
     </div>
   )
 }
