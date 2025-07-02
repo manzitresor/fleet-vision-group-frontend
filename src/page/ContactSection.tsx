e73c8d6e4fec102f3daff962914a8a3ed0025676
manzitresor <manzitre@gmail.com>
2025-07-03 00:58:09 +0200

    chore: add styles for contact page

--- src/page/ContactSection.tsx
+++ src/page/ContactSection.tsx
@@ -1,7 +1,9 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import { useForm } from "react-hook-form"
 import type { SubmitHandler } from "react-hook-form"
+import withKeys from '../assets/withKey.svg'
 
 type Inputs = {
   example: string
@@ -12,20 +14,23 @@ export default function ContactSection() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
   } = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
   return (
-    <div>
+    <div className='py-10 bg-gray-100'>
         <div className=" flex flex-col items-center justify-center">
             <h4 className="text-2xl">Contact us</h4>
             <hr className="w-31 h-px my-3 bg-main-orange border-0"></hr>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Input defaultValue="test" {...register("example")} />
-          <Input {...register("exampleRequired", { required: true })} />
-          <Button></Button>
-        </form>
-    </div>
+        <div className='flex flex-row items-center justify-center gap-4 lg:gap-10 bg-white p-7 mx-64'>
+          <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 w-lg'>
+            <Input placeholder='Your name' {...register("example")} className='border' />
+            <Input placeholder='Your email' {...register("exampleRequired", { required: true })} />
+            <Textarea placeholder='Your message' />
+            <Button className='bg-main-orange text-white cursor-pointer hover:bg-black'>Send a message</Button>
+          </form>
+          <img src={withKeys} alt="with keys" className='w-60 h-auto'/>
+        </div>
+      </div>
   )
 }
